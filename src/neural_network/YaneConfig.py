8e4b784917ad61241a433b41486ff506e4655e2d
Downvote Hunter <25457206+Downvote-Hunter@users.noreply.github.com>
2023-11-04 14:45:55 +0100

    Update YaneConfig.py

--- src/neural_network/YaneConfig.py
+++ src/neural_network/YaneConfig.py
@@ -65,8 +65,12 @@ def get_population_size(json_config):
     return json_config["population_size_reference"]
 
 
-def get_population_survival_rate(json_config):
-    return json_config["population_survival_rate"]
+def get_mutation_bias_probability(json_config):
+    return json_config["mutation_bias_probability"]
+
+
+def get_mutation_shift_probability(json_config):
+    return json_config["mutation_shift_probability"]
 
 
 def create_default_json_config():
@@ -76,14 +80,16 @@ def create_default_json_config():
         # propagation. If false, yane will always execute forward propagation until the end.
         "allow_early_output": True,
         "clear_on_new_input": True,  # if true, yane will reset all neuron values when a new input is given
-        "weight_shift": [0.01, 0.1],  # How much the weight can change shift in one direction. [min, max]
+        "weight_shift": [0.1, 0.1],  # How much the weight can change shift in one direction. [min, max]
         "mutation_random_weight": [-2, 2],  # The range of the random weight when mutating
         "activation_functions": ["Tanh", "ReLU", "Sigmoid", "Binary", "Linear"],  # all activation functions
         "binary_threshold": 0.5,  # only used for binary activation function
-        "mutation_connection_probability": 0.5,  # chance that a new connection is created
-        "mutation_neuron_probability": 0.5,  # chance that a new neuron is created
-        "mutation_weight_probability": 0.5,  # chance that a weight is mutated
-        "mutation_activation_function_probability": 0.5,  # chance that an activation function is mutated
+        "mutation_connection_probability": 0.1,  # chance that a new connection is created
+        "mutation_bias_probability": 0.1,  # chance that a bias is mutated
+        "mutation_neuron_probability": 0.1,  # chance that a new neuron is created
+        "mutation_weight_probability": 0.1,  # chance that a weight is mutated
+        "mutation_shift_probability": 0.1,  # chance that a weight is shifted
+        "mutation_activation_function_probability": 0.1,  # chance that an activation function is mutated
         "population_size_reference": 100,  # The approximate amount of genomes in a population. May fluctuate.
 
         # The factor that is multiplied with the net cost to calculate the fitness.
