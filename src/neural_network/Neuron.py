3f884b483b739d3d6cf8a166c7009665f70162d8
Downvote Hunter <25457206+Downvote-Hunter@users.noreply.github.com>
2023-11-05 13:18:09 +0100

    Remove bias

--- src/neural_network/Neuron.py
+++ src/neural_network/Neuron.py
@@ -1,6 +1,10 @@
 from copy import deepcopy
 
+from src.neural_network import YaneConfig
 from src.neural_network.ActivationFunction import ActivationFunction
+from src.neural_network.exceptions.InvalidConnection import InvalidConnection
+
+yane_config = YaneConfig.load_json_config()
 
 
 class Neuron:
@@ -8,30 +12,22 @@ class Neuron:
 
     def __init__(self):
         self.value = 0.0
-        self.bias = 1.0
         self.next_connections = []
         self.activation = ActivationFunction.SIGMOID
         self.id = Neuron.ID
         Neuron.ID += 1
 
     def __str__(self):
-        return "Neuron: " + str(self.id) + " Value: " + str(self.value) + " Bias: " + str(
-            self.bias) + " Activation: " + str(self.activation)
+        return "Neuron: " + str(self.id) + " Value: " + str(self.value) + " Activation: " + str(self.activation)
 
     def set_value(self, value):
         self.value = value
-
-    def set_bias(self, bias):
-        self.bias = bias
 
     def set_activation(self, activation):
         self.activation = activation
 
     def get_value(self):
         return self.value
-
-    def get_bias(self):
-        return self.bias
 
     def get_activation(self):
         return self.activation
@@ -40,6 +36,19 @@ class Neuron:
         return self.next_connections
 
     def add_next_connection(self, connection):
+        if connection in self.next_connections:
+            raise InvalidConnection("Cannot add connection twice")
+
+        if connection.get_in_neuron() != self:
+            raise InvalidConnection("Cannot add connection with different in neuron than this neuron")
+
+        if connection.get_out_neuron() is None:
+            raise InvalidConnection("Cannot add connection with no out neuron")
+
+        for next_connection in self.next_connections:
+            if next_connection.get_out_neuron() == connection.get_out_neuron():
+                raise InvalidConnection("Cannot add connection with same out neuron twice")
+
         self.next_connections.append(connection)
 
     def activate(self):
@@ -61,5 +70,5 @@ class Neuron:
             next_neuron: Neuron = connection.get_out_neuron()
             next_neuron.set_value(next_neuron.get_value() + self.value * connection.get_weight())
 
-    def set_connections(self, connections):
-        self.next_connections = connections
+    def mutate_activation_function(self):
+        self.activation = ActivationFunction.get_function(YaneConfig.get_random_activation_function(yane_config))
