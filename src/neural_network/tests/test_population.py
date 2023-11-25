1d01824fcbf6c8a4757b6f500817c80af0a9717b
Downvote Hunter <25457206+Downvote-Hunter@users.noreply.github.com>
2023-11-25 03:05:14 +0100

    Create test_population.py

---
+++ src/neural_network/tests/test_population.py
@@ -0,0 +1,30 @@
+def test_add_genome():
+    assert False
+
+
+def test_pop_genome():
+    assert False
+
+
+def test_remove_genome():
+    assert False
+
+
+def test_get_genomes():
+    assert False
+
+
+def test_get_size():
+    assert False
+
+
+def test_sort():
+    assert False
+
+
+def test_get_average_fitness():
+    assert False
+
+
+def test_add_output_neuron():
+    assert False
