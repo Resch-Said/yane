1c8f3effc1854293245b70768442610f7cfa3d97
Downvote Hunter <25457206+Downvote-Hunter@users.noreply.github.com>
2023-12-03 21:03:41 +0100

    Add cartpole example

---
+++ src/examples/gym/CartPole.py
@@ -0,0 +1,56 @@
+import gym
+import numpy as np
+
+from src.neural_network.Genome import Genome
+from src.neural_network.NeuroEvolution import NeuroEvolution
+
+env = gym.make('CartPole-v1', render_mode="human")
+
+yane = NeuroEvolution()
+yane.set_number_of_outputs(2)
+yane.set_min_fitness(500)
+
+
+def evaluate_normal_input(genome: Genome):
+    state = env.reset()
+    state = state[0]
+    done = False
+    fitness = 0
+    while not done:
+        input_data = list(state)
+
+        outputs = genome.forward_propagation(input_data)
+        action = np.argmax(outputs)
+
+        state, reward, done, _, _ = env.step(action)
+        fitness += reward
+
+    return fitness
+
+
+yane.train(evaluate_normal_input)
+
+yane.print()
+best_genome = yane.get_best_species_genome()[1]
+
+print()
+print(best_genome.print())
+print()
+
+env = gym.make('CartPole-v1', render_mode="human")
+
+state = env.reset()
+state = state[0]
+done = False
+fitness = 0
+while not done:
+    input_data = list(state)
+
+    outputs = best_genome.forward_propagation(input_data)
+    action = np.argmax(outputs)
+
+    state, reward, done, _, _ = env.step(action)
+
+best_genome.plot()
+
+env.close()
