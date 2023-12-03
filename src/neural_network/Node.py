d5a6c51ec24909f956b0c2d1cbfa6e6fc1cc3ac3
Downvote Hunter <25457206+Downvote-Hunter@users.noreply.github.com>
2023-12-03 15:35:12 +0100

    randomize activation function

--- src/neural_network/Node.py
+++ src/neural_network/Node.py
@@ -14,7 +14,7 @@ class Node:
     def __init__(self, node_type: NodeTypes, ID=None):
         self.value = 0.0
         self.next_connections = []
-        self.activation = ActivationFunction.LINEAR
+        self.activation = ActivationFunction.get_function(YaneConfig.get_random_activation_function(yane_config))
         self.type = node_type
         self.id = ID
 
